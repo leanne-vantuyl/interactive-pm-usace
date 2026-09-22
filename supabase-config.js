9ad9bd94bf3f4efe859d9042432eb3fe312e6d83
leanne-vantuyl <leanne.vantuyl@gmail.com>
2026-09-22 11:39:52 -0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = 'https://uvzgrrypvjuxmzytdyvs.supabase.co';
+
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_3un3K44EQlu3n_1W8TzhpA_laikZo6h';
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
